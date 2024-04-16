a6b8260356e5c8e106af5d1d34fd188085e2b687
a2ron-k <aaronkwah@gmail.com>
2024-04-16 21:53:26 +0200

    CC-03: add unit animation frames and movement

--- Units/unit.gd
+++ Units/unit.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 @export var selected = false
 @onready var box = get_node("Box")
+@onready var target = position
+@onready var animation = get_node("AnimationPlayer")
+
+var followCursor = false
+var speed = 50
+
 
 func _ready():
 	setSelected(selected)
@@ -9,7 +15,30 @@ func _ready():
 
 func setSelected(value): 
 	box.visible = value
+	selected = value
+	
 
+# Handles input
+func _input(event):
+	if(event.is_action_pressed("RightClick")):
+		followCursor = true
+	
+	if(event.is_action_released("RightClick")):
+		followCursor = false
+	pass
 
-func _on_input_event(viewport, event, shape_idx):
-	pass # Replace with function body.
+# Handles the click and move
+func _physics_process(delta):
+	if followCursor:
+		if selected:
+			target = get_global_mouse_position()
+			animation.play("WalkDown")
+	
+	velocity = position.direction_to(target) * speed
+	
+	if position.distance_to(target) > 15:
+		move_and_slide()
+	else:
+		animation.stop()
+	
+
