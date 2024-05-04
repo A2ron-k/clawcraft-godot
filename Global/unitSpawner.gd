1aee8ee790aa82fd5672898fed57c97e29b3b170
a2ron-k <aaronkwah@gmail.com>
2024-05-04 12:38:33 +0200

    CC-45: update unit spawning UI and buttons

--- Global/unitSpawner.gd
+++ Global/unitSpawner.gd
@@ -11,60 +11,38 @@ var totalTime = 50 #5 secs
 var currentTime = 0
 
 # Units & Buttons
-@onready var gatherer = preload("res://Units/Gatherer.tscn")
-@onready var spawnGathererBtn = $HBoxContainer/Gatherer
+@onready var catMarine = preload("res://Units/RangeAttacker.tscn") 
+@onready var spawnCatMarineBtn = $HBoxContainer/Marine
+@onready var catMarineSprite = $HBoxContainer/CatMarineSprite
+@onready var spawnMeleeBtn = $HBoxContainer/Melee
+@onready var meleeSprite = $HBoxContainer/ChiwawaSprite
 
-var gathererUnitSpawnCost = 2
-#var meleeUnitSpawnCost = 4
-#var rangeUnitSpawnCost = 5
-#var heavyUnitSpawnCost = 5
+var marineUnitSpawnCost = 5
 
 func _process(delta):
 	enableButtons()
 
-func _on_gatherer_pressed():
-	var worldPath = get_tree().get_root().get_node("World")
-	var gathererPath = get_tree().get_root().get_node("World/Gatherers")
-	var basePath = get_tree().get_root().get_node("World/HomeBase/Base")
-	var gathererCount = gathererPath.get_child_count() # Count how many gathers there are... 
-	
-	# Check how much Catnips the person has, if have enough then spawn
-	if Game.Catnip >= gathererUnitSpawnCost:
-		Game.Catnip -= gathererUnitSpawnCost
-		
-		startTimer()
-		await get_tree().create_timer(4.9).timeout
-		
-		var newGatherer = gatherer.instantiate()
-		newGatherer.name = "Gatherer" + str(gathererCount + 1) # name them accordingly
-		
-		# Spawning in front of the base
-		var randomPosX = randi_range(-10,10)
-		var randomPosY = randi_range(30,50)
-		newGatherer.position = basePath.position + Vector2(randomPosX, randomPosY)
-		
-		# Add the units to the respective nodes and arrays
-		gathererPath.add_child(newGatherer)
-		worldPath.getUnits()
-	else:
-		# Display the notication
-		Game.notification = "Not Enough Catnips to spawn a Gatherer"
-		await get_tree().create_timer(3.0).timeout
-		Game.notification = ""
-
-	
 # Disables all buttons
 func disableAllButtons():
-	spawnGathererBtn.disabled = true
+	spawnCatMarineBtn.disabled = true
+	spawnMeleeBtn.disabled = true
+	
 
 # Enables all buttons if they meet the conditions
 func enableButtons():
 	# TODO - Add any new buttons
+	
+	# TODO - Remove the 2 lines below after new units are introduced
+	spawnMeleeBtn.disabled = true
+	meleeSprite.modulate = Color(0.286, 0.286, 0.286)
+	
 	if isTimerStarted == false:
-		if Game.Catnip >= gathererUnitSpawnCost:
-			spawnGathererBtn.disabled = false
+		if Game.Catnip >= marineUnitSpawnCost:
+			spawnCatMarineBtn.disabled = false
+			catMarineSprite.modulate = Color(1, 1, 1)
 		else:
-			spawnGathererBtn.disabled = true
+			spawnCatMarineBtn.disabled = true
+			catMarineSprite.modulate = Color(0.286, 0.286, 0.286)
 
 # Handles the "when" to disable of button
 func startTimer():
@@ -88,3 +66,33 @@ func _on_tick_timer_timeout():
 	var tween = get_tree().create_tween()
 	tween.tween_property(cooldownBar, "value", currentTime,0.1).set_trans(Tween.TRANS_QUAD) # Animate Value in the Bar
 
+
+func _on_marine_pressed():
+	var worldPath = get_tree().get_root().get_node("World")
+	var rangeAttackersPath = get_tree().get_root().get_node("World/RangeAttackers")
+	var barrackPath = get_tree().get_root().get_node("World/Buildings/Barrack")
+	var rangeAttackersCount = rangeAttackersPath.get_child_count() # Count how many rangeAttackers there are... 
+	
+	# Check how much Catnips the person has, if have enough then spawn
+	if Game.Catnip >= marineUnitSpawnCost:
+		Game.Catnip -= marineUnitSpawnCost
+		
+		startTimer()
+		await get_tree().create_timer(4.9).timeout
+		
+		var newCatMarine = catMarine.instantiate()
+		newCatMarine.name = "CatMarine" + str(rangeAttackersCount + 1) # name them accordingly
+		
+		# Spawning in front of the base
+		var randomPosX = randi_range(-10,10)
+		var randomPosY = randi_range(30,50)
+		newCatMarine.position = barrackPath.position + Vector2(randomPosX, randomPosY)
+		
+		# Add the units to the respective nodes and arrays
+		rangeAttackersPath.add_child(newCatMarine)
+		worldPath.getUnits()
+	else:
+		# Display the notication
+		Game.notification = "Not Enough Catnips to spawn a Cat Marine"
+		await get_tree().create_timer(3.0).timeout
+		Game.notification = ""
